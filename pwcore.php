6db12994b85f3665a6fc2be1a5532936480abcde
Brian Tuju <briantuju@gmail.com>
2023-01-30 18:36:42 +0300

    Initial commit

---
+++ pwcore.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: PW Core
+Plugin URI: https://example.com
+Description: Core plugin for Publish Wiki Pages
+Version: 0.1.0
+Author: Brian Tuju
+Author URI: https://github.com/briantuju
+Text Domain: pwpages
+*/
+
+if (!defined('ABSPATH')) {
+  die('NOPE! You cannot be here.');
+}
