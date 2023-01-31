d714c6d38639e57e4f047db6d05b92c635552661
Brian Tuju <briantuju@gmail.com>
2023-01-31 10:14:36 +0300

    Add order metabox

--- includes/orders/create-order.php
+++ includes/orders/create-order.php
@@ -8,6 +8,8 @@ add_action('rest_api_init', 'pwcore_create_rest_endpoints');
 
 add_action('init', 'pwcore_create_orders_page');
 
+add_action('add_meta_boxes', 'pwcore_create_meta_box');
+
 function pwcore_create_order_form()
 {
   include PW_PLUGIN_PATH . './includes/templates/orders/new-order-form.php';
@@ -22,6 +24,34 @@ function pwcore_enqueue_assets()
   );
 }
 
+function pwcore_create_meta_box()
+{
+  add_meta_box(
+    'pw_orders_meta_box',
+    'Order Details',
+    'pwcore_show_order',
+    'pw_orders'
+  );
+}
+
+function pwcore_show_order()
+{
+  // Get ALL meta data
+  $post_metas = get_post_meta(get_the_ID());
+
+  // Get SINGLE meta data entry
+  $order_topic = get_post_meta(get_the_ID(), 'topic', true);
+
+  echo "<h1>" . $order_topic . "</h1><br/>";
+
+  unset($post_metas['_edit_last']);
+  unset($post_metas['_edit_lock']);
+
+  foreach ($post_metas as $key => $value) {
+    echo "<strong>" . ucfirst($key) . "</strong>" . " => {$value[0]} <br/> <br/>";
+  }
+}
+
 function pwcore_create_orders_page()
 {
   $args = [
@@ -29,9 +59,15 @@ function pwcore_create_orders_page()
     'has_archive' => true,
     'labels' => [
       'name' => 'Orders',
-      'singular_name' => 'Order'
+      'singular_name' => 'Order',
+      'edit_item' => ''
     ],
-    'supports' => ['title', 'editor', 'author', 'custom-fields']
+    'supports' => false,
+    'capability_type' => 'post',
+    'capabilities' => [
+      'create_posts' => false
+    ],
+    'map_meta_cap' => true
   ];
 
   register_post_type('pw_orders', $args);
@@ -65,6 +101,9 @@ function pwcore_store_order(WP_REST_Request $data)
   $message = "<p style='font-size: larger; color: brown'>A new order has been placed</p>";
   $subject = "New Order";
 
+  // Clean unwanted data before saving
+  unset($params['_wpnonce']);
+  unset($params['_wp_http_referer']);
   pwcore_save_order($params);
 
   wp_mail($admin_mail, $subject, $message, $headers);
@@ -76,7 +115,8 @@ function pwcore_save_order(array $data)
 {
   $order_data = [
     'post_title' => $data['topic'],
-    'post_type' => 'pw_orders'
+    'post_type' => 'pw_orders',
+    'post_status' => 'publish'
   ];
 
   $post_id = wp_insert_post($order_data);
