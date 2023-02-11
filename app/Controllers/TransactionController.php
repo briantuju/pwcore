b62db67101d8092debfdfe9263cc0a5683928484
Brian Tuju <briantuju@gmail.com>
2023-02-11 10:24:49 +0300

    send order payment received email

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -5,7 +5,10 @@ namespace PWCore\Controllers;
 use PWCore\Enums\InvoiceStatus;
 use PWCore\Enums\OrderStatus;
 use PWCore\Enums\TransactionStatus;
+use PWCore\Services\EmailOptions;
 use PWCore\Services\InvoiceService;
+use PWCore\Services\Mail\EmailService;
+use PWCore\Services\Orders\OrderOptions;
 use PWCore\Services\Orders\OrderService;
 
 class TransactionController {
@@ -14,9 +17,18 @@ class TransactionController {
 
   protected InvoiceService $invoice_service;
 
+  protected EmailOptions $email_options;
+
+  protected OrderOptions $order_options;
+
+  protected EmailService $email_service;
+
   public function __construct() {
 	$this->order_service   = new OrderService;
+	$this->email_options   = new EmailOptions;
+	$this->order_options   = new OrderOptions;
 	$this->invoice_service = new InvoiceService;
+	$this->email_service   = new  EmailService;
   }
 
   public function store( array $data ) {
@@ -25,6 +37,7 @@ class TransactionController {
 	$order        = get_post( $data['order_id'] );
 	$invoice      = get_post( $data['invoice_id'] );
 	$order_number = get_post_meta( $order->ID, 'order_number', true );
+	$amount       = get_post_meta( $invoice->ID, 'amount', true );
 
 	$transaction_data = [
 		'post_title'     => "Order " . $order_number . " payment",
@@ -37,7 +50,7 @@ class TransactionController {
 
 	$post_id = wp_insert_post( $transaction_data );
 
-	$data['amount']  = get_post_meta( $invoice->ID, 'amount', true );
+	$data['amount']  = $amount;
 	$data['user_id'] = $user->ID;
 	add_post_meta( $post_id, 'transaction_status', TransactionStatus::COMPLETED->value );
 
@@ -49,6 +62,20 @@ class TransactionController {
 	// Update order status and invoice status
 	$this->order_service->update_status( $order, OrderStatus::IN_PROGRESS );
 	$this->invoice_service->update_status( $invoice, InvoiceStatus::PAID );
+
+	// Send payment received email
+	$order_mail = pwcore_get_theme_options( $this->order_options->get_order_email() );
+	$email      = pwcore_get_theme_options(
+		$this->email_options->get_option_order_pay_received()
+	);
+	$email      = str_replace( "[name]", $user?->user_login, $email );
+	$email      = str_replace( "[amount]", $amount, $email );
+	$email      = str_replace( "[order_number]", $order_number, $email );
+	$this->email_service->send_email(
+		$user,
+		"Order payment received",
+		$email, "Support<$order_mail>"
+	);
 
 	return get_post( $post_id );
   }
