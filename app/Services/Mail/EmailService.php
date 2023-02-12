63c1e33d2cb27bbcbea92bf38598418e907fea31
Brian Tuju <briantuju@gmail.com>
2023-02-12 20:56:15 +0300

    Update email layout

--- app/Services/Mail/EmailService.php
+++ app/Services/Mail/EmailService.php
@@ -2,6 +2,7 @@
 
 namespace PWCore\Services\Mail;
 
+use DateTime;
 use PWCore\Services\EmailOptions;
 use WP_User;
 
@@ -48,12 +49,20 @@ class EmailService {
 	  string $from = null,
 	  string $reply_to = null,
   ): void {
-	$headers   = [];
-	$headers[] = "From: " . $from ?? self::get_from_address();
-	$headers[] = "Reply-to: " . $reply_to ?? "no-reply<" . self::get_from_mail() . ">";
-	$headers[] = "Content-Type: text/html";
+	$headers       = [];
+	$headers[]     = "From: " . $from ?? self::get_from_address();
+	$headers[]     = "Reply-to: " . $reply_to ?? "no-reply<" . self::get_from_mail() . ">";
+	$headers[]     = "Content-Type: text/html";
+	$date          = new DateTime( 'now' );
+	$year          = $date->format( 'Y' );
+	$site_name     = get_bloginfo( 'name' );
+	$custom_header = "<p style='padding: 2rem 1rem; background-color:#e5e7eb; border-top-left-radius: 1rem; border-top-right-radius: 1rem'></p>";
+	$custom_footer = "<p style='padding: 2rem 1rem; background-color:#e5e7eb; border-bottom-left-radius: 1rem; border-bottom-right-radius: 1rem; text-align: center'>&copy; "
+					 . $year . " $site_name. All rights reserved</p>";
+	$message       = $custom_header . "<div style='padding: 0 1rem'>"
+					 . $email . "</div>" . $custom_footer;
 
-	wp_mail( $user->user_email, $subject, $email, $headers );
+	wp_mail( $user->user_email, $subject, $message, $headers );
   }
 
   /**
